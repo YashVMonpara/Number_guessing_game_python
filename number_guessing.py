ecb16510f93f9d8ba010e45cdc7a16f663d4d134
YashVMonpara <yashmonpara11@gmail.com>
2021-05-25 21:07:05 +0530

    Number Guessing game

---
+++ number_guessing.py
@@ -0,0 +1,17 @@
+import random
+
+guesses = 0
+random_num = random.randint(0, 100)
+
+while guesses < 10:
+
+    guessed_num = int(input("Enter Number: "))
+
+    if guessed_num == random_num:
+        print('You Won!!')
+        break
+    elif guessed_num < random_num:
+        print('Guess is low')
+    else:
+        print('Guess is high')
+    guesses += 1
